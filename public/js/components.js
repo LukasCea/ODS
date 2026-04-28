dda7dca206591a9e06d5ab8c780481a577e4dde0
LukasCea <lukas.ceasandoval.7e9@itb.cat>
2026-04-28 08:26:06 +0200

    build: implementacion de los enlaces a las paginas ODS

--- public/js/components.js
+++ public/js/components.js
@@ -4,7 +4,16 @@ function loadHeader() {
     header.innerHTML = `
         <nav class="navbar">
             <a href="index.html">Inicio</a>
-            <a href="pagina2.html">ODS 1</a>
+            <a href="pages/ODS1.html">ODS 1</a>
+            <a href="pages/ODS2.html">ODS 2</a>
+            <a href="pages/ODS3.html">ODS 3</a>
+            <a href="pages/ODS4.html">ODS 4</a>
+            <a href="pages/ODS5.html">ODS 5</a>
+            <a href="pages/ODS6.html">ODS 6</a>
+            <a href="pages/ODS7.html">ODS 7</a>
+            <a href="pages/ODS8.html">ODS 8</a>
+            <a href="pages/ODS9.html">ODS 9</a>
+            <a href="pages/ODS10.html">ODS 10</a>
             </nav>
     `;
 }
