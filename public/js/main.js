6e39ad48b8bacfe740b7dff12d747784064f5272
LukasCea <lukas.ceasandoval.7e9@itb.cat>
2026-05-26 13:04:45 +0200

    feat: update personal description and improve registration form placeholders for clarity

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ async function cargarPaginaInicial() {
 
         if (contenedorPersonal && infoPersonal) {
             contenedorPersonal.innerHTML = `
-            <img src="https://www.pixartprinting.it/blog/wp-content/uploads/2021/06/1_Mona_Lisa_300ppi.jpg" alt="Mona Lisa">
+            <img src="img/lukas.png" alt="Lukas Cea Sandoval">
                 <article>
                     <h2>Descripcion personal</h2>
                     <p>${infoPersonal.descripcion}</p>
